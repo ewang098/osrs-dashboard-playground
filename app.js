d0ea610966977490c20f71a5fddd3c487c1862b4
ewang098 <ewang098@yahoo.com>
2026-01-15 19:32:51 -0600

    feat: incorporate osrs api

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const button = document.getElementById("loadBtn");
 const resultDiv = document.getElementById("result");
 
+// TODO: move https://runescape.wiki/w/Application_programming_interface to readme
+
+const BASE_ROUTE =  "https://secure.runescape.com/m=itemdb_oldschool/api/";
+
+const ITEM_MAP = new Map(
+    // bars
+    ["addy_bar", 2361]
+    ["rune_bar", 2363],
+    // ores
+    ["addy_ore", 449],
+    ["rune_ore", 451],
+    // equipables
+    ["addy_platebody", 1123],
+    ["rune_2h_sword", 1319],
+);
+
+const ROUTE_MAP = new Map(
+    ["item_details", "catalogue/detail.json?item="],
+)
+
 button.addEventListener("click", async () => {
     try {
-        // Fetch a public API
-        const response = await fetch("https://api.chucknorris.io/jokes/random");
+        // make route
+        const addyBarRoute = BASE_ROUTE + ROUTE_MAP.get("item_details") + ITEM_MAP.get("addy_bar");
+        
+        // Fetch
+        const response = await fetch(addyBarRoute);
         const data = await response.json();
 
         // Display the result
